e2217524d3432df0233fb95f017ec13246a08d7c
Clara Marinho <119897667+mclaramarinho@users.noreply.github.com>
2023-12-09 11:54:09 -0300

    Delete back/api/setup.py

--- back/api/setup.py
+++
@@ -1,9 +0,0 @@
-firebaseConfig = {
-  "apiKey": "AIzaSyAP-ytff2KHx1EkKDjnncLu2EWx5SL2EPc",
-  "authDomain": "zeitask.firebaseapp.com",
-  "projectId": "zeitask",
-  "storageBucket": "zeitask.appspot.com",
-  "messagingSenderId": "708185522867",
-  "appId": "1:708185522867:web:4d0cde2fe490945e2ee6c6",
-  "measurementId": "G-4LC53J2RGY"
-}
