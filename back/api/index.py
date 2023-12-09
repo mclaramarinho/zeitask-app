e8899fdf3bbe5c69a6377cb5707ecf8461754f1c
mclaramarinho <marinho.claramb@gmail.com>
2023-12-09 11:52:22 -0300

    safe

--- back/api/index.py
+++ back/api/index.py
@@ -4,13 +4,31 @@ import firebase_admin
 from firebase_admin import credentials, auth
 import pyrebase
 from setup import firebaseConfig
+from decouple import config
 
+
+private_key_id = config('FIREBASE_PRIVATE_KEY_ID')
+private_key = config('FIREBASE_PRIVATE_KEY')
+project_id = config('FIREBASE_PROJECT_ID')
 
 app = Flask(__name__)
 
 fb = pyrebase.initialize_app(firebaseConfig)
 auth = fb.auth()
-cred = credentials.Certificate('methods/setup1.json')
+#cred = credentials.Certificate('methods/setup1.json')
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": f"{project_id}",
+    "private_key_id": f"{private_key_id}",
+    "private_key": f"{private_key}",
+    "client_email": "firebase-adminsdk-66v42@zeitask.iam.gserviceaccount.com",
+    "client_id": "101692490798999468550",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-66v42%40zeitask.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+})
 firebase_admin.initialize_app(cred)
 
 
