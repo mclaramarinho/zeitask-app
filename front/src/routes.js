14c8b1837c702b3a59a8b491637b79760c64df63
mclaramarinho <marinho.claramb@gmail.com>
2023-12-12 16:39:19 -0300

    feat: added new routes

--- front/src/routes.js
+++ front/src/routes.js
@@ -3,6 +3,7 @@ import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import Dashboard from './pages/Dashboard';
+import ToDoPage from './pages/ToDoPage';
 
 const routes = createBrowserRouter([
     {
@@ -30,6 +31,15 @@ const routes = createBrowserRouter([
     {
         path: '/dashboard/:id/profile',
         //go to profile page
+    },
+    {
+        path: '/dashboard/:id/todo',
+        //go to to do page
+        element: <ToDoPage />
+    },
+    {
+        path: '/dashboard/:id/kanban',
+        //go to kanban page
     }
 ])
 
