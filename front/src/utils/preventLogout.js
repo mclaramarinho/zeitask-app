ce22fcfbc6f186bc042942e355856012f01d8da1
mclaramarinho <marinho.claramb@gmail.com>
2023-12-14 16:52:59 -0300

    feat: prevent logout module

---
+++ front/src/utils/preventLogout.js
@@ -0,0 +1,45 @@
+import { isUserSignedIn, whoIsSignedIn } from "../firebase/auth"
+import Navigate from "./navigate"
+
+async function preventLogout(whereAt, setIsSignedIn){
+
+    return new Promise((resolve, reject) => {
+        isUserSignedIn().then(r => {
+            if(r){
+                whoIsSignedIn().then(res => {
+                    setIsSignedIn && setIsSignedIn(true);
+                    resolve(handlePath(whereAt, res))    
+                })
+            }else{
+                console.log(r)
+                setIsSignedIn && setIsSignedIn(false);
+                resolve('/login')
+            }
+        })
+    })
+}
+
+function handlePath(whereAt, id){
+    let response = "";
+    switch(whereAt){
+        case 'login':
+        case 'dashboard':
+            response = `/dashboard/${id}`;
+            break;
+        case 'todo':
+            response = `/dashboard/${id}/todo`;
+            break;
+        case 'kanban':
+            response = `/dashboard/${id}/kanban`;
+            break;
+        case 'profile':
+            response = `/dashboard/${id}/profile`;
+            break;
+    }
+    return response;
+}
+
+
+
+
+export {preventLogout}
