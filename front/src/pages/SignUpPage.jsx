413bd382a3b62f8913f467c4e4f31085be0d0249
mclaramarinho <marinho.claramb@gmail.com>
2023-12-16 10:33:50 -0300

    feat: signup page done

--- front/src/pages/SignUpPage.jsx
+++ front/src/pages/SignUpPage.jsx
@@ -1,35 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import LogoHeader from "../components/LogoHeader";
 import { Box, Checkbox, FormControl, FormControlLabel, TextField } from "@mui/material";
 import { checkRegex } from "../utils/checkRegex";
 import PasswordRules from "../components/PasswordRules";
 import Button from "../components/Button";
-import { checkEmailAvailability } from "../firebase/auth";
+import { checkEmailAvailability, checkUsernameAvailability } from "../firebase/db/user";
+import { signUp, signIn } from "../firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { preventLogout } from "../utils/preventLogout";
 
 function SignUpPage(){
+    const navigate = useNavigate();
+
     const [email, setEmail] = useState("");
-    const [isEmailValid, setIsEmailValid] = useState();
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const [isEmailAvailable, setIsEmailAvailable] = useState(true);
+
 
     const [username, setUserName] = useState("");
-    const [isUsernameValid, setIsUsernameValid] = useState();
+    const [isUsernameValid, setIsUsernameValid] = useState(true);
+    const [isUsernameAvailable, setIsUsernameAvailable] = useState(true);
 
-
+    
     const [password, setPassword] = useState("");
-    const [isPswdValid, setIsPswdValid] = useState();
+    const [isPswdValid, setIsPswdValid] = useState(true);
     const [showPswdRules, setShowPswdRules] = useState();
 
     const [confirmPswd, setConfirmPswd] = useState("");
-    const [pswdMatch, setPswdMatch] = useState();
-
+    const [pswdMatch, setPswdMatch] = useState(true);
     
+    const [isTermAccepted, setIsTermAccepted] = useState()
+
+    const [isRequiredEmpty, setIsRequiredEmpty] = useState()
+    const [errorSigningUp, setErrorSigningUp] = useState()
+
     function handleEmail(e){
         setEmail(e.target.value)
         setIsEmailValid(checkRegex("email", e.target.value));
+        !isEmailAvailable && !checkRegex("email", e.target.value) && setIsEmailAvailable(true)
     }
 
     function handleUsername(e){
         setUserName(e.target.value)
         setIsUsernameValid(checkRegex("username", e.target.value));
+        !isUsernameAvailable && !checkRegex("username", e.target.value) && setIsUsernameAvailable(true)
     }
 
     function handlePswd(e){
@@ -46,18 +60,47 @@ function SignUpPage(){
     }
 
     function handleSubmit(){
-        //check regex
-        //check availability of username and email in database
-        // console.log(checkEmailAvailability(email))
+        if(email === undefined && username === undefined && password === undefined && isTermAccepted ===undefined){
+            setIsRequiredEmpty(true)
+            return;
+        }
+        if(email.length === 0 || username.length === 0 || password.length === 0 || !isTermAccepted){
+            setIsRequiredEmpty(true)
+            return;
+        }
+
+        if(checkRegex("email", email)){
+            setIsEmailValid(true)
+            checkEmailAvailability(email).then(r => {
+                setIsEmailAvailable(r)
+            })
+        }else{
+            setIsEmailValid(false)
+        }
+
+        if(checkRegex("username", username)){
+            setIsUsernameValid(true)
+            checkUsernameAvailability(username).then(r => {
+                setIsUsernameAvailable(r)
+            })
+        }else{
+            setIsUsernameValid(false);
+        }
+
+        signUp(email, password, isTermAccepted, username)
+        .then(r=>{
+            signIn(email, password).then((r) => {
+                r && preventLogout('signup').then(p => navigate(p))
+            })
+            setErrorSigningUp(false)
+        })
+        .catch(e =>{
+            setIsRequiredEmpty(false)
+            setErrorSigningUp(true)
+        })
+        
     }
 
-    function checkEmailDB(){
-        //checks if the email already exists in the database
-    }
-
-    function checkUsernameDB(){
-        //checks if the username already exists in the database
-    }
     
 
     return(
@@ -75,26 +118,32 @@ function SignUpPage(){
                             <div className="row">
                                 <TextField id="email" variant="standard" fullWidth
                                             error = {isEmailValid !== undefined && !isEmailValid}
-                                            helperText={isEmailValid !== undefined && !isEmailValid && "The email informed is not in the correct format."}
+                                            
                                             color={isEmailValid ? "success" : "warning"}
                                             value={email}
                                             type="email"  label="Email"
                                             onChange={(e) => handleEmail(e)}/>
+                                {isEmailAvailable && isEmailValid && <p></p>}
+                                {!isEmailValid && <p className="error-msg m-0">The email informed is not in the correct format.</p>}
+                                {!isEmailAvailable && <p className="error-msg m-0">This email is already exists!</p>}
                             </div>
                             <div className="row mt-md-5">
                                 <TextField id="username" variant="standard" fullWidth
                                             error = {isUsernameValid !== undefined && !isUsernameValid}
-                                            helperText = {isUsernameValid !== undefined && !isUsernameValid && "The username can only contain letters (A to Z), numbers (0 to 9), underline (_) and dash (-)."}
                                             color={isUsernameValid ? "success" : "warning"}
                                             value={username}
                                             type="email"  label="Username"
                                             onChange={(e) => handleUsername(e)} />
+                                {isUsernameAvailable && isUsernameValid && <p></p>}
+                                {!isUsernameAvailable && <p className="error-msg p-0 m-0">This username already exists!</p>}
+                                {!isUsernameValid && <p className="error-msg m-0">The username can only contain letters (A to Z), numbers (0 to 9), underline (_) and dash (-).</p>}
                             </div>
+                            
                         </div>
 
                         <div className="col-12 col-sm-11 col-md-4 col-lg-3 mx-auto">
                             <div className="row">
-                                    <TextField id="email-field" variant="standard" fullWidth
+                                    <TextField  variant="standard" fullWidth
                                             error = {isPswdValid !== undefined && !isPswdValid}
                                             color={isPswdValid ? "success" : "warning"}
                                             
@@ -103,15 +152,17 @@ function SignUpPage(){
                                             onChange={(e) => handlePswd(e)} onFocus={() => setShowPswdRules(true)} onBlur={() => setShowPswdRules(false)}/>
                                     
                                     {showPswdRules && <PasswordRules pswd={password} />}
+                                    <p></p>
                             </div>
                             <div className="row mt-md-5">
-                                <TextField id="email-field" variant="standard" fullWidth
+                                <TextField  variant="standard" fullWidth
                                         error = {pswdMatch !== undefined && !pswdMatch}
                                         color={pswdMatch ? "success" : "warning"}
                                         helperText = {pswdMatch !== undefined && !pswdMatch && "Your passwords do not match!"}
                                         value={confirmPswd}
                                         type="password"  label="Confirm Password" 
                                         onChange={(e) => handleConfirmPswd(e)} />
+                                        <p></p>
                             </div>
                         </div>
                         <div className="col-12 col-sm-11 col-md-1 col-lg-1 col-xl-2 mx-auto"></div>
@@ -121,10 +172,14 @@ function SignUpPage(){
                                 label={"By continuing the registration, you hereby agree to this software's Terms and Conditions and its Privacy Policy."}
                                 className="mx-auto p-0 m-0 text-start justify-content-center f-12 mt-5"
                                 disableTypography
+                                onChange={(e) => setIsTermAccepted(e.target.checked)}
                             />
                         </div>
                         <div className="row gutter-x-0">
+                            
                             <Button label={"DONE!"} color={"green"} size="large" classes={"col-12 col-sm-6 col-md-3 mx-auto mt-5 bolder"} action = {handleSubmit} />
+                            {isRequiredEmpty && <p className="fields-required-msg">All fields are required!</p>}
+                            {errorSigningUp && <p className="fields-required-msg">This account already exists! <a href="/login">Sign in!</a></p>}
                         </div>
                     </div>
                     
