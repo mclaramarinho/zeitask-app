7686ee4a5ee8c60abe85c94cc1f681893b7eb719
mclaramarinho <marinho.claramb@gmail.com>
2023-12-16 10:33:26 -0300

    feat: signin, signup, signout

--- front/src/firebase/auth.js
+++ front/src/firebase/auth.js
@@ -1,18 +1,36 @@
-import { browserSessionPersistence, getAdditionalUserInfo, getAuth, onAuthStateChanged, setPersistence, signInWithCustomToken, signInWithEmailAndPassword, updateCurrentUser} from "firebase/auth";
+import { browserLocalPersistence, onAuthStateChanged, setPersistence, signInWithEmailAndPassword, signOut, Auth, createUserWithEmailAndPassword, signInWithCredential, updateCurrentUser, updateProfile} from "firebase/auth";
 import { app, auth } from "./setup";
 import { getLoginCookies, setLoginCookies } from "../utils/loginCookies";
+import { createNewUser } from "./db/user";
+
+
+async function signUp(email, password, terms, username){
+
+        try{
+            const userCred = await createUserWithEmailAndPassword(auth, email, password)
+            const user = userCred.user;
+            await updateProfile(auth.currentUser, {
+                displayName: username
+            })
+            const result = await createNewUser(email, username, terms)
+            return true;
+        }catch(err){
+            throw err.message;
+        }
+}
+
+
 
 async function signIn (email, password){
 
     const result = await new Promise((resolve, reject) => {
         
-        setPersistence(auth, browserSessionPersistence)
+        setPersistence(auth,  browserLocalPersistence)
         .then(async () => {
             
             signInWithEmailAndPassword(auth, email, password)
             .then((userCred) => {
-                const user = userCred.user;
-                resolve(user);
+                resolve();
             })
             .catch(err =>{
                 resolve("INVALID CREDENTIALS");
@@ -21,9 +39,33 @@ async function signIn (email, password){
             resolve("AN ERROR OCCURRED")
         })
     })
+
+    const ls = Object.keys(localStorage);
+    let persistenceLS = "";
+    ls.forEach(item => {
+        if(item.includes('DEFAULT')){
+            persistenceLS = item;
+        }
+    })
+    let currentData = JSON.parse(localStorage.getItem(persistenceLS));
+    currentData.apiKey = null;
+
+    localStorage.setItem(persistenceLS, JSON.stringify(currentData))
     
     return (result!=="INVALID CREDENTIALS" && (result!=="AN ERROR OCCURRED"))
 }
+
+async function signUserOut(){
+    try{
+        signOut(auth).then(r=>{
+            return true
+        })
+        
+    }catch(err){
+        return false
+    }
+}
+
 
 // checks if theres an user signed in 
 async function isUserSignedIn(){
@@ -63,4 +105,4 @@ function getUserInfo(uid){
 
 
 
-export {signIn, getUserInfo, isUserSignedIn, whoIsSignedIn};
+export {signIn, getUserInfo, isUserSignedIn, whoIsSignedIn, signUserOut, signUp};
