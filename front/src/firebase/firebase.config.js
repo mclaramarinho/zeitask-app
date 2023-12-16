ddc3c9765b3f9da694d60ce285622c2c31715257
mclaramarinho <marinho.claramb@gmail.com>
2023-12-16 10:41:12 -0300

    A

---
+++ front/src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  
+  apiKey: process.env.API_KEY,
+  authDomain: "zeitask.firebaseapp.com",
+  projectId: "zeitask",
+  storageBucket: "zeitask.appspot.com",
+  messagingSenderId: "708185522867",
+  appId: "1:708185522867:web:4d0cde2fe490945e2ee6c6",
+  measurementId: "G-4LC53J2RGY",
+  databaseURL: "https://zeitask-default-rtdb.firebaseio.com",
+};
+
+export default firebaseConfig;
