89669fe14a080308777e346f58d58a84595ca81d
mclaramarinho <marinho.claramb@gmail.com>
2023-12-17 16:49:25 -0300

    Add additional CSS stylesheets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../src/styles/index.css';
+import '../src/styles/backgrounds.css';
+import '../src/styles/bootstrapReset.css';
+import '../src/styles/fonts.css';
+import '../src/styles/fontTemplate.css';
+import '../src/styles/lists.css';
+import '../src/styles/responsivity.css';
+import '../src/styles/sizing.css';
 import { RouterProvider } from 'react-router-dom';
 import routes from './routes';
 
