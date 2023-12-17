8dea4c5a96b9e1ef532df17e0c03c0f9102caaaf
mclaramarinho <marinho.claramb@gmail.com>
2023-12-17 19:30:56 -0300

    feat: Add updatePassword and sendEmailVerification functions

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,4 +1,4 @@
-import { browserLocalPersistence, onAuthStateChanged, setPersistence, signInWithEmailAndPassword, signOut, Auth, createUserWithEmailAndPassword, signInWithCredential, updateCurrentUser, updateProfile} from "firebase/auth";
+import { browserLocalPersistence, onAuthStateChanged, setPersistence, signInWithEmailAndPassword, signOut, Auth, createUserWithEmailAndPassword, signInWithCredential, updateCurrentUser, updateProfile, updatePassword, sendEmailVerification} from "firebase/auth";
 import { app, auth } from "./setup";
 import { getLoginCookies, setLoginCookies } from "../utils/loginCookies";
 import { createNewUser } from "./db/user";
@@ -80,11 +80,24 @@ async function isUserSignedIn(){
     })
 }
 
-async function whoIsSignedIn(){
+async function whoIsSignedIn(what){
+    
     return new Promise((resolve, reject) => {
         onAuthStateChanged(auth, (user) => {
             if(user){
-                resolve(user.displayName)
+                if(what === "username"){
+                    resolve(user.displayName)
+                }else if(what === "email"){
+                    resolve(user.email)
+                }else if(what === "verified"){
+                    resolve(user.emailVerified)
+                }else if(what === "user"){
+                    resolve(user)
+                }                
+                else{
+                    resolve(user.displayName)
+                }
+                
             }else{
                 resolve("NO USER SIGNED IN")
             }
@@ -93,16 +106,45 @@ async function whoIsSignedIn(){
 }
 
 
-function getUserInfo(uid){
-    const user = auth.currentUser;
-    if(user !== null){
-        const displayName = user.displayName;
-        return displayName;
-    }else{
-        return false;
-    }
+async function getUsername(){
+
+    return new Promise((resolve, reject) => {
+        whoIsSignedIn("username").then(r => {resolve(r)})
+    })
+}
+
+function getEmail(){
+    return new Promise((res, rej) => {
+        whoIsSignedIn("email").then(r => res(r))
+    })
+}
+
+function isEmailConfirmed(){
+    return new Promise((res, rej) =>{
+        whoIsSignedIn("verified").then(r => res(r))
+    })
+}
+
+//unfinished
+async function changePassword(newPassword){
+    const user = await whoIsSignedIn("user");
+    return updatePassword(user, newPassword).then(() => {
+        return true;
+    })
+    .catch((err) => {
+        return false
+    })
+    // updateCurrentUser(auth, user)
+}
+
+async function sendVerification(){
+    const user = await whoIsSignedIn("user");
+    return sendEmailVerification(user).then(() => true).catch(err => false)
+}
+
+async function changeEmail(newEmail){
+    
 }
 
 
-
-export {signIn, getUserInfo, isUserSignedIn, whoIsSignedIn, signUserOut, signUp};
+export {signIn, getUsername, isUserSignedIn, whoIsSignedIn, signUserOut, signUp, getEmail, isEmailConfirmed, changePassword, sendVerification};
