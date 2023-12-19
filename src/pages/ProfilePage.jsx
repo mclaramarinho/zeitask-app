d26dfb39b3b23df9d7b4a600f9271f4723a3ffea
mclaramarinho <marinho.claramb@gmail.com>
2023-12-19 10:01:32 -0300

    feat: change email linked to account

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { redirect, useNavigate, useParams } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import { IconButton, TextField, inputAdornmentClasses } from "@mui/material";
-import { changePassword, getEmail, getUsername, isEmailConfirmed, sendVerification } from "../firebase/auth";
-import { SaveButton } from "react-admin";
+import { changeEmail, changePassword, getEmail, getUsername, isEmailConfirmed, sendVerification } from "../firebase/auth";
 import {Edit, Save} from "@mui/icons-material"
+import { checkEmailAvailability } from "../firebase/db/user";
+import { checkRegex } from "../utils/checkRegex";
+import { preventLogout } from "../utils/preventLogout";
+
 function ProfilePage(){
     const {id} = useParams();
+    const navigate = useNavigate();
     const [email, setEmail] = useState("");
+    const [isEmailAvailable, setIsEmailAvailable] = useState(true);
+    const [isEmailCorrect, setIsEmailCorrect] = useState(true);
+
     const [username, setUsername] = useState("");
+    const [isUsernameAvailable, setIsUsernameAvailable] = useState(true);
+
     const [isVerified, setIsVerified] = useState(false);
+
+    const [password, setPassword] = useState("");
 
     const [isEditingEmail, setIsEditingEmail] = useState(false);
     const [isEditingUsername, setIsEditingUsername] = useState(false);
@@ -17,6 +28,7 @@ function ProfilePage(){
 
     const [sentEmail, setSentEmail] = useState(false);
     useEffect(() => {
+        preventLogout('profile').then(r => navigate(r))
         getUsername().then(r => setUsername(r))
         getEmail().then(r => setEmail(r))
         isEmailConfirmed().then(r => setIsVerified(r))
@@ -26,14 +38,37 @@ function ProfilePage(){
         sendVerification().then(r => setSentEmail(r))
     }
 
-    function handleEditEmail(){
+    async function handleEditEmail(){
+
         if(!isEditingEmail){
+            setIsEditingEmail(true)
             console.log("editing")
+        }else{
+            const currentEmail = await getEmail();
+            if(email !== currentEmail){
+                if(checkRegex("email", email)){
+                    checkEmailAvailability(email).then(r => {
+                        setIsEmailAvailable(r);
+                        if(r){
+                            setIsEditingEmail(false)
+                            sessionStorage.setItem("newEmail", email);
+                            navigate("/reauthenticate/email");
+                        }
+                    })
+                }else{
+                    setIsEmailCorrect(false);
+                }
+            }else{
+                setIsEmailAvailable(true)
+                setIsEditingEmail(false)
+                setIsEmailAvailable(true)
+                setIsEmailCorrect(true)
+            }
+            
         }
-        // else{
-        // //     if
-        // // }
     }
+    useEffect(() => {
+    }, [isEmailAvailable])
     return (
         <div className="container-fluid p-0">
             <NavBar />
@@ -42,8 +77,8 @@ function ProfilePage(){
                 <div className="row h-75 my-auto">
                     <div className="col-12 col-md-6 mx-auto m-md-0">
                         <div className="container">
-                            <TextField label={"EMAIL"} value={email} disabled={!isEditingEmail} onChange={(e) => setEmail(e.target.value)} className="mb-2 col-11" />
-                            <IconButton className="col-1" onMouseUp={() => {setIsEditingEmail(!isEditingEmail); handleEditEmail()}}>{!isEditingEmail ? <Edit /> : <Save />} </IconButton>
+                            <TextField helperText={!isEmailAvailable ? "This email is not available!" : !isEmailCorrect && "This is not an email!"} label={"EMAIL"} value={email} disabled={!isEditingEmail} onChange={(e) => setEmail(e.target.value)} className="mb-2 col-11" />
+                            <IconButton className="col-1" onMouseUp={() => {handleEditEmail()}}>{!isEditingEmail ? <Edit /> : <Save />} </IconButton>
                             {isVerified && <p>Your email is verified!</p>}
                             {!isVerified && sentEmail && <p>Verification email sent!</p>}
                             {!isVerified && !sentEmail && <p className="pointer-cursor" onMouseUp={() => handleEmailVerification()}>Verify your email now!</p>}  
