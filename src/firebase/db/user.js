0c1cb724f93d3c8ff9a349b01fa73f30e8006907
mclaramarinho <marinho.claramb@gmail.com>
2023-12-19 16:50:00 -0300

    feat: get user db info + update user db info

--- src/firebase/db/user.js
+++ src/firebase/db/user.js
@@ -47,4 +47,46 @@ async function createNewUser(email, username, terms){
 
 }
 
-export {checkEmailAvailability, checkUsernameAvailability, createNewUser}
+async function getUserDBInfo(user){
+    const username = user.displayName;
+    const actualRef = ref(db, `users/${username}`);
+    try{
+        const snapshot = await get(actualRef);
+        return snapshot.val();
+    }catch(error){
+        throw error;
+    }
+}
+
+async function updateUserDatabase(whatToUpdate, newValue){
+    let actualRef;
+    const username = await whoIsSignedIn("username");
+    return new Promise(async (res, rej) => {
+        if(whatToUpdate === 'email'){
+            actualRef = ref(db, 'users/'+username+'/email');
+            try{
+                await set(actualRef, newValue);
+                res(true);
+            }catch(error){
+                res(false);
+            }
+        }else if(whatToUpdate === 'username'){
+            actualRef = ref(db, 'users');
+            try{
+                const snapshot = await get(actualRef);
+                snapshot.forEach(item => {
+                    if(item.key === username){
+                        item.key = newValue;
+                        item.val()['username'] = newValue;
+                    }
+                })
+                res(true);
+            }catch(error){
+                res(false);
+            }
+        }
+    })
+   
+}
+
+export {checkEmailAvailability, checkUsernameAvailability, createNewUser, updateUserDatabase, getUserDBInfo}
