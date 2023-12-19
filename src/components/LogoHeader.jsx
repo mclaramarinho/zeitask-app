f69bffd4fef36821287a07918e4299fd58b63eaa
mclaramarinho <marinho.claramb@gmail.com>
2023-12-19 17:31:18 -0300

    feat: click LogoHeader component to go to main page

--- src/components/LogoHeader.jsx
+++ src/components/LogoHeader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from '../assets/zeitask-logo-no-bg.png'
+import { useNavigate } from "react-router-dom";
 
 function LogoHeader(){
+    const navigate = useNavigate();
     return(
         <div className="py-5 text-center position-relative">
-            <img src={logo} alt="Zeitask logo" className="m-auto position-relative MW-90"/>
+            <img onMouseUp={()=>navigate('/')} src={logo} alt="Zeitask logo" className="m-auto position-relative MW-90"/>
         </div> 
     )
 }
